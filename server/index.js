f3cfd869e34ab854c4c93c8273c95ee26cdfab5a
frepingonsalvese <94340132+frepingonsalvese@users.noreply.github.com>
2024-01-14 15:03:45 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use("/admin", adminRouter)
 app.use("/user", userRouter)
 
-app.get('/'  , (req , res) => {res.json(msg : "Hi")})
+app.get('/'  , (req , res) => {res.json(msg : "Hi brother ")})
 // Connect to MongoDB
 // DONT MISUSE THIS THANKYOU!!
 mongoose.connect('mongodb+srv://frepingonsalvese13:jszRR3zEp9F7ZRXT@cluster0.pnjdotk.mongodb.net', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "NewCourse" });
